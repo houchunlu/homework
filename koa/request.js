679eb31cc355de4b138c6685e45fee1f980cee10
houchunlu <1577188317@qq.com>
2018-12-30 18:17:32 +0800

    作业-koa源码

---
+++ koa/request.js
@@ -0,0 +1,18 @@
+let url = require('url')
+let request = { 
+  get url(){
+    return this.req.url;
+  },
+  get path(){
+    let { pathname } = url.parse(this.req.url);
+    return pathname
+  },
+  get query() {
+    let { query } = url.parse(this.req.url,true);
+    return query
+  }
+};
+
+
+
+module.exports = request;
